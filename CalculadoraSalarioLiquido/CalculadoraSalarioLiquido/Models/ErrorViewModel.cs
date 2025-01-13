e7528bbc006812dfd4af5c1147b2f43d8ae96036
vinic <viniciusrbastos@hotmail.com>
2025-01-13 11:08:11 -0300

    Feat: criando arquitetura base do projeto.

---
+++ CalculadoraSalarioLiquido/CalculadoraSalarioLiquido/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CalculadoraSalarioLiquido.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
